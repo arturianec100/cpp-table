3dc591ed751a5b40112096ead9243d5e834f8549
arturianec100 <arturianec100@gmail.com>
2021-02-12 23:11:12 +0200

    add ru_UA translation

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,11 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
+    QTranslator translator;
+    translator.load(QString("cpp-table_") + QLocale::system().name());
+    app.installTranslator(&translator);
     MainWindow w;
     w.show();
-    return a.exec();
+    return app.exec();
 }
---
+++ translations/cpp-table_ru_UA.ts
@@ -0,0 +1,135 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_UA">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>MainWindow</source>
+        <translation type="vanished">Главное окно</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="14"/>
+        <source>C++ Table</source>
+        <translation>C++ Таблица</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="42"/>
+        <source>toolBar</source>
+        <translation>Панель инструментов</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="30"/>
+        <source>Message</source>
+        <translation>Сообщение</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="68"/>
+        <location filename="../src/mainwindow.cpp" line="104"/>
+        <source>Open File</source>
+        <translation>Открыть файл</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="70"/>
+        <source>Save File</source>
+        <translation>Сохранить файл</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="106"/>
+        <location filename="../src/mainwindow.cpp" line="115"/>
+        <source>All files (*.*)</source>
+        <translation>Все файлы (*.*)</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="113"/>
+        <source>Save to File</source>
+        <translation>Сохранить как</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="141"/>
+        <source>Table not found in file %1</source>
+        <translation>Таблица не найдена в файле %1</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="142"/>
+        <source>File parsing error:
+%1</source>
+        <translation>Ошибка парсинга файла:
+%1</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="166"/>
+        <source>Can&apos;t open file %1</source>
+        <translation>Невозможно открыть файл %1</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="170"/>
+        <source>Can&apos;t save to file %1</source>
+        <translation>Невозможно сохранить в файл %1</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="122"/>
+        <source>File %1 not found.</source>
+        <translation>Файл %1 не найден</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="10"/>
+        <source>Table Tools</source>
+        <translation>Инструменты таблицы</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="146"/>
+        <source>Can&apos;t read file %1</source>
+        <translation>Невозможно прочитать файл %1</translation>
+    </message>
+</context>
+<context>
+    <name>TableTools</name>
+    <message>
+        <location filename="../src/tabletools.ui" line="19"/>
+        <source>Form</source>
+        <translation>Форма</translation>
+    </message>
+    <message>
+        <location filename="../src/tabletools.ui" line="25"/>
+        <source>Row</source>
+        <translation>Строка</translation>
+    </message>
+    <message>
+        <location filename="../src/tabletools.ui" line="42"/>
+        <location filename="../src/tabletools.ui" line="94"/>
+        <source>Remove Selected</source>
+        <translation>Удалить выделенное</translation>
+    </message>
+    <message>
+        <location filename="../src/tabletools.ui" line="49"/>
+        <location filename="../src/tabletools.ui" line="101"/>
+        <source>Add Here</source>
+        <translation>Добавить сюда</translation>
+    </message>
+    <message>
+        <location filename="../src/tabletools.ui" line="56"/>
+        <source>Add on Top</source>
+        <translation>Добавить сверху</translation>
+    </message>
+    <message>
+        <location filename="../src/tabletools.ui" line="63"/>
+        <source>Add Bottom</source>
+        <translation>Добавить снизу</translation>
+    </message>
+    <message>
+        <location filename="../src/tabletools.ui" line="77"/>
+        <source>Column</source>
+        <translation>Столбец</translation>
+    </message>
+    <message>
+        <location filename="../src/tabletools.ui" line="108"/>
+        <source>Add Left</source>
+        <translation>Добавить слева</translation>
+    </message>
+    <message>
+        <location filename="../src/tabletools.ui" line="115"/>
+        <source>Add Right</source>
+        <translation>Добавить справа</translation>
+    </message>
+</context>
+</TS>
